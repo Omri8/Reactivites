71961bfe5f03d3c0c86f714a90bf02ee30d8c98e
omri <omriufc@me.com>
2023-09-02 13:43:33 +0300

    End Of Section 6

--- client-app/src/features/activites/form/ActivityForm.tsx
+++ client-app/src/features/activites/form/ActivityForm.tsx
@@ -6,12 +6,14 @@ interface Props {
   activity: Activity | undefined;
   closeForm: () => void;
   createOrEdit: (activity: Activity) => void;
+  submitting: boolean;
 }
 
 export default function ActivityForm({
   activity: selectedActivity,
   closeForm,
   createOrEdit,
+  submitting,
 }: Props) {
   const initialState = selectedActivity ?? {
     id: '',
@@ -38,7 +40,7 @@ export default function ActivityForm({
 
   return (
     <Segment clearing>
-      <Form onSubmit={handleSubmit} autoComplete='off'>
+      <Form onSubmit={handleSubmit}>
         <Form.Input
           placeholder='Title'
           value={activity.title}
@@ -54,10 +56,11 @@ export default function ActivityForm({
         <Form.Input
           placeholder='Category'
           value={activity.cateory}
-          name='Category'
+          name='cateory'
           onChange={handleInputChange}
         />
         <Form.Input
+          type='date'
           placeholder='Date'
           value={activity.date}
           name='date'
@@ -66,16 +69,22 @@ export default function ActivityForm({
         <Form.Input
           placeholder='City'
           value={activity.city}
-          name='City'
+          name='city'
           onChange={handleInputChange}
         />
         <Form.Input
           placeholder='Venue'
           value={activity.venue}
-          name='Venue'
+          name='venue'
           onChange={handleInputChange}
         />
-        <Button floated='right' positive type='submit' content='Submit' />
+        <Button
+          loading={submitting}
+          floated='right'
+          positive
+          type='submit'
+          content='Submit'
+        />
         <Button
           onClick={closeForm}
           floated='right'
